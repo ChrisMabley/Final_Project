65be71000a125240f8df750c78a4ea4ae34c2e52
ChrisMabley <59846867+ChrisMabley@users.noreply.github.com>
2022-12-16 16:39:32 -0500

    push

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let searchText = document.getElementByID('searchbar').value;
-let searchBtn = document.getElementByID('searchbutton').addEventListener('click',search());
+let searchButton = document.getElementByID('searchbutton').addEventListener('click',search());
 
 async function search(){
   let results = await axios.get(`https://www.fishwatch.gov/api/species`)
   console.log(results);
+  show(results);
 }
+
+function show(results){
+    let tab = 
+        <tr>
+            <th>Species Name</th>
+            <th>Scientific Name</th>
+            <th>Habitat</th>
+            <th>Image Gallery</th>
+        </tr>
+
+    
